cedc380cd5ec42dfe32270c72960ee6e6a2949ad
NickLegacy <nicklegacy13@gmail.com>
2022-08-19 19:16:30 -0400

    initial commit

---
+++ VariableExercise/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace VariableExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string dogName = "Ralph";
+            int dogAge = 10;
+            Console.WriteLine($"My dog's name is {dogName}, he is {dogAge} years old.");
+        }
+    }
+}
